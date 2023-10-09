f07b3170151c80f6c83f9ea26aa8e50d95cd128d
Soel Attala <soelattala7@gmail.com>
2023-10-09 17:56:49 -0300

    hosteamos en smarterASP.net

--- TaskApp/Models/TaskAppContext.cs
+++ TaskApp/Models/TaskAppContext.cs
@@ -19,8 +19,12 @@ public partial class TaskAppContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=SOEL-PC\\SQLEXPRESS; DataBase=TaskApp;Integrated Security=true; Encrypt=False;");
-
+        => optionsBuilder.UseSqlServer("Data Source=SQL8002.site4now.net;Initial Catalog=db_a9ff42_taskapp;User Id=db_a9ff42_taskapp_admin;Password=prueba123456");
+    /*
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+    => optionsBuilder.UseSqlServer("Server=SOEL-PC\\SQLEXPRESS; DataBase=TaskApp;Integrated Security=true; Encrypt=False;");
+    */
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Tasks>(entity =>
