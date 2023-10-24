eca953f31a0143c0e16a210b872f79a6d373a1a1
Soel Attala <soelattala7@gmail.com>
2023-10-24 08:57:22 -0300

    Create new project, adding packages and DB connection

---
+++ ContactsApp/Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContactsApp.Models;
+
+public partial class Contact
+{
+    public int ContactId { get; set; }
+
+    public string? ContactName { get; set; }
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+}
---
+++ ContactsApp/Models/ContactDbContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactsApp.Models;
+
+public partial class ContactDbContext : DbContext
+{
+    public ContactDbContext()
+    {
+    }
+
+    public ContactDbContext(DbContextOptions<ContactDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Contact> Contacts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=SOEL-PC\\SQLEXPRESS; Encrypt=False; DataBase=ContactDB;Integrated Security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Contact>(entity =>
+        {
+            entity.HasKey(e => e.ContactId).HasName("PK__Contact__7121FD150B9D7638");
+
+            entity.ToTable("Contact");
+
+            entity.Property(e => e.ContactId).HasColumnName("contactID");
+            entity.Property(e => e.ContactName)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("contactName");
+            entity.Property(e => e.Email)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("email");
+            entity.Property(e => e.Phone)
+                .HasMaxLength(50)
+                .IsUnicode(false)
+                .HasColumnName("phone");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
