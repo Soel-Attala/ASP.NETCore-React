ec17ccc0724235470da864e88eb287463da5b19f
Soel Attala <soelattala7@gmail.com>
2023-11-06 14:43:52 -0300

    adding business view and controller

---
+++ SalesSystem/SalesSystem.WebApp/Controllers/CategoriesController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesSystem.WebApp.Controllers
+{
+    public class CategoriesController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
