c7e10e646b711b74a21ce568a24403e05e973a71
Soel Attala <soelattala7@gmail.com>
2023-11-13 20:51:14 -0300

    dependencies injection update

--- SalesSystem/SalesSystem.IOC/Dependencies.cs
+++ SalesSystem/SalesSystem.IOC/Dependencies.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SalesSystem.DAL.DBContext;
 using Microsoft.EntityFrameworkCore;
-using System.Runtime.CompilerServices;
 using Microsoft.Extensions.Configuration;
-//using SalesSystem.DAL.Interfaces;
-//using SalesSystem.DAL.Implementations;
+using SalesSystem.DAL.Interfaces;
+using SalesSystem.DAL.Implementations;
 //using SalesSystem.BLL.Implementations;
 //using SalesSystem.BLL.Interfaces;
 
@@ -29,6 +23,10 @@ namespace SalesSystem.IOC
                 options.UseSqlServer(configuration.GetConnectionString("SQLChain"));
 
             });
+
+
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddScoped<ISalesRepository, SalesRepository>();
         }
     }
 }
